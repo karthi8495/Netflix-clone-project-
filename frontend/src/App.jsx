0e04e86fa54289ca902034460f36489ea5a2d461
karthikeyan <karthikeyanmadhappan@gmail.com>
2026-01-28 22:28:20 +0530

    update

---
+++ frontend/src/App.jsx
@@ -0,0 +1,37 @@
+import React, { useEffect } from 'react'
+import { Route,Routes } from 'react-router-dom';
+import HomePage from './pages/home/HomePage';
+import LoginPage from './pages/LoginPage';
+import SignUpPage from './pages/SignUpPage';
+import Footer from './components/Footer';
+import { Toaster } from 'react-hot-toast';
+import { useAuthStore } from './store/authUser';
+
+function App() {
+  const {user, isCheckingAuth, authCheck } = useAuthStore();
+  console.log("auth user is here:", user);
+
+  useEffect(() => {
+    authCheck();
+  },[]);
+
+  
+  return (
+    <>
+      <Routes>
+        <Route path='/' element={<HomePage/>}/>
+        <Route path='/login' element={<LoginPage/>}/>
+        <Route path='/signup' element={<SignUpPage/>}/>
+      </Routes>
+      <Footer />
+
+      <Toaster />
+      
+
+    </>
+  );
+    
+  
+}
+
+export default App
